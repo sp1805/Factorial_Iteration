cc1421bf078895294b92c456b8940bd03d7e5077
Shailesh Prajapati <shailesh.welcome@gmail.com>
2018-07-10 12:51:02 +0530

    Factorial of a number
    Using iteration method

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            if (n >= 0)
+                Factorial(n);
+            
+        }
+
+        public static void Factorial(int n)
+        {
+            long result = 1;
+            if (n == 0)
+                result = 1;
+            if (n > 1)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    result = result * i;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
